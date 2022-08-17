115bc8030cd46ca53191b018f248d119d386edd9
largomst <dingrui.zhen@gmail.com>
2022-08-17 22:06:53 +0800

    feat: basic view now return minimal HTML

--- lists/tests.py
+++ lists/tests.py
@@ -16,6 +16,6 @@ class HomePageTest(TestCase):
         request = HttpRequest()
         response = home_page(request)
         html = response.content.decode("utf-8")
-        self.assertTrue(html.startswith("html"))
+        self.assertTrue(html.startswith("<html>"))
         self.assertIn("<title>To-Do lists</title>", html)
         self.assertTrue(html.endswith("</html>"))
